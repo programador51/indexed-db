2cb8f03d25ae3b49c09832295adcbcc742679bac
programador51 <gskitcat@gmail.com>
2021-08-14 19:17:43 -0500

    feat: Seccion 90 - Manejo de errores e insercion de registros

--- app.js
+++ app.js
@@ -13,3 +13,47 @@ request.onupgradeneeded = e => {
         keyPath:'id'
     });
 }
+
+// Handle errors
+request.onerror = e => {
+    console.error('DB Error:',e.target.error);
+}
+
+// Insert data
+request.onsuccess = e => {
+    const db = e.target.result;
+
+    const heroesData = [
+        {
+            id:'111',
+            heroe:'spider-man',
+            message:'Mary Jane!!'
+        },
+        {
+            id:'222',
+            heroe:'iron-man',
+            message:'Pepper ?'
+        }
+    ];
+
+    const heroesTransaction = db.transaction('heroes','readwrite');
+
+    heroesTransaction.onerror = e => {
+        console.error('Error saving',e.target.error);
+    }
+
+    // Alert of success insertion
+    heroesTransaction.oncomplete = e => {
+        console.log('Transaction success',e);
+    }
+
+    const heroesStore = heroesTransaction.objectStore('heroes');
+
+    for(let heroe of heroesData){
+        heroesStore.add(heroe);
+    }
+
+    heroesStore.onsuccess = e => {
+        console.log('Item added');
+    }
+}
