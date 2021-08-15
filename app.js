6b1138f44f34fd226ca8baf13cb2c22c5850f3fe
programador51 <gskitcat@gmail.com>
2021-08-14 19:06:08 -0500

    feat: Seccion 89 - Inicio en indexedDB

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-
 // indexedDB: Reforzamiento
 
+// Create space of the DB
+const request = window.indexedDB.open('my-database',1);
 
+// BD updates when: create or update of version
+request.onupgradeneeded = e => {
+    console.log('Update of db');
 
+    const db = e.target.result;
 
+    db.createObjectStore('heroes',{
+        keyPath:'id'
+    });
+}
